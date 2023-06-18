6c5a13c73638456733d23d80c531a2843b1b82a5
Dennis Kennetz <drkennetz@gmail.com>
2023-06-18 14:25:41 -0500

    adding cname

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,6 +15,7 @@ module.exports = function(eleventyConfig) {
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/",
+		"./CNAME": "/",
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
 	});
 
